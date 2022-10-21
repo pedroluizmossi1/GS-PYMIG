49e42b10be04ab38ab66cdd5cdd56e048b40e2f1
pedroluizmossi1 <79431501+pedroluizmossi1@users.noreply.github.com>
2022-10-21 17:11:03 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,99 @@
+import cx_Oracle
+from email.policy import default
 import eel
 from ctypes.wintypes import CHAR
 import psycopg2
 import pandas as pd
 from tkinter import *
 from tkinter import ttk
-import cx_Oracle
+
 import sys
 import os
+import configparser
 
 # name of folder where the html, css, js, image files are located
 eel.init('templates')
 
-# Connect to your postgres DB
-#conn = psycopg2.connect(host='10.1.28.30', 
-#                         database='BARATAO',
-#                         user='postgres', 
-#                         password='91396851')
-
-# Open a cursor to perform database operations
-#cur = conn.cursor()
-
-# Execute a query
-#cur.execute("SELECT * FROM public.unidade_med")
-
-# Retrieve query results
-#records = cur.fetchall()
-
-#df = pd.DataFrame(records)
-#print(df[0][0])
 
 
+# Configuçao do config.ini
 
-#Variavel de ambiente do Instant Client - Inicio
+config = configparser.ConfigParser()
+config.read('config.ini')
+default_config = config['DEFAULT']
 
+
+config.read('config.ini')
+print(default_config.get('InstantClient'))
+# Configuçao do config.ini
+
+
+# Variavel de ambiente do Instant Client - Inicio
 try:
     if sys.platform.startswith("darwin"):
         lib_dir = os.path.join(os.environ.get("HOME"), "Downloads",
                                "instantclient_19_8")
         cx_Oracle.init_oracle_client(lib_dir=lib_dir)
     elif sys.platform.startswith("win32"):
-        lib_dir=r"instantclient_21_7"
+        lib_dir = (r'' + default_config.get('InstantClient'))
         cx_Oracle.init_oracle_client(lib_dir=lib_dir)
 except Exception as err:
     print("Whoops!")
-    print(err);
-    sys.exit(1);
+    print(err)
+    sys.exit(1)
 
-#Variavel de ambiente do Instant Client - FIM
 
-#Abertura da Conexao ORACLE
 @eel.expose
-def connect_oracle(ora_login,ora_password,ora_host,ora_port,ora_service):
+def read_parm_instant_client():
+    return default_config.get('InstantClient')
+
+
+# Abertura da Conexao ORACLE
+@eel.expose
+def connect_oracle(ora_login, ora_password, ora_host, ora_port, ora_service):
 
     dsn_tns = cx_Oracle.makedsn(ora_host, ora_port, service_name=ora_service)
     global con
     con = cx_Oracle.connect(user=ora_login, password=ora_password, dsn=dsn_tns)
     print(con.version)
-    ora_conectado = 'Conectado'
+    try:
+        ora_conectado = 'Conectado'
+    except BaseException as e:
+        ora_conectado = e
     return ora_conectado
+
+# Abertura da Conexao POSTGRES
+
+#Connect to your postgres DB
+@eel.expose
+def connect_postgres(host,database,port,user,password):
+    
+    global con_pos
+    con_pos = psycopg2.connect(host=host,
+                    database=database,
+                    user=user,
+                    password=password)
+    print('Postgres conectado')
+
+# Execute a query
+@eel.expose
+def select_seco():
+    cur_pos = con_pos.cursor()
+    cur_pos.execute("SELECT * FROM public.unidade_med")
+    records = cur_pos.fetchall()
+    df = pd.DataFrame(records)
+    print(df[0][0])
+
+
+
 
 
 @eel.expose
 def insert_unid():
     print('Migrado')
     cur = con.cursor()
-    cur.execute("insert into unid_medid(um_sig,um_nom,dat_alter) values ('KD','KD',sysdate)")
+    cur.execute(
+        "insert into unid_medid(um_sig,um_nom,dat_alter) values ('KD','KD',sysdate)")
     con.commit()
 
 
@@ -78,6 +107,93 @@ def ora_version():
     return rows
 
 
+@eel.expose
+def parm_ora_con(user, host, port, sid):
+    cfgfile = open('config.ini', 'w')
+    config.set('ORA_CON', 'user', user)
+    config.set('ORA_CON', 'host', host)
+    config.set('ORA_CON', 'port', port)
+    config.set('ORA_CON', 'sid', sid)
+    config.write(cfgfile)
+    cfgfile.close()
+
+
+@eel.expose
+def read_parm_ora_con():
+    return config.get('ORA_CON', 'user'), config.get('ORA_CON', 'host'), config.get('ORA_CON', 'port'), config.get('ORA_CON', 'sid')
+
+
+@eel.expose
+def parm_instant_client(parm_instant_client_value,ora_user,ora_host,ora_port,ora_sid):
+    cfgfile = open('config.ini', 'w')
+    config.set('DEFAULT', 'InstantClient', parm_instant_client_value)
+    config.set('ORA_CON', 'user', ora_user)
+    config.set('ORA_CON', 'host', ora_host)
+    config.set('ORA_CON', 'port', ora_port)
+    config.set('ORA_CON', 'sid', ora_sid)
+    config.write(cfgfile)
+    cfgfile.close()
+
+
+
+
+
+# Fechar conexao ORACLE
+@eel.expose
+def ora_con_close():
+    con.close()
+    print('Conexao Oracle Fechada')
+
+# Fechar conexao POSTGRES
+@eel.expose
+def pos_con_close():
+    con_pos.close()
+    print('Conexao Postgres Fechada')
+
+#Select para validor os modulos da migração
+
+def to_tuple(first_output):
+        series = []
+        for py_tuple in first_output:
+            series.append(py_tuple[0])
+        return tuple(series)
+
+@eel.expose
+def ora_mod_select():
+    cur = con.cursor()
+    cur.arraysize = 100
+    cur.execute("""select ao.procedure_name from all_procedures ao
+                                where ao.object_name like '%P_PYMIG%'
+                                and ao.object_type = 'PACKAGE'
+                                and ao.procedure_name is not null
+                                """)                           
+    employee=[]
+    for i in cur:
+        employee.append(i[0])
+    print(employee)
+    cur.close()  
+    return(employee)
+    
+
+
+
+#Insert Unidades de medida oracle
+df = (['KD', 'KI'],['KD', 'KI'])
+
+@eel.expose
+def ora_mod_un_medid():
+    cur = con.cursor()
+    len_sql = (len(df))
+    len_start = 0
+    while len_start < len_sql:
+
+        cur = con.cursor() 
+        myvar = df[0][len_start]
+        myvar2 = df[1][len_start]
+        cur.callproc('gondola.p_pymig_import_un_medid', (myvar,myvar2)) 
+        len_start = len_start+1
+        cur.close() 
+    con.commit()
 
 # 1000 is width of window and 600 is the height
-eel.start('index.html', mode='default', size=(1000, 600)) 
+eel.start('index.html', mode='default', size=(1000, 600))
